724a593795f9b92f2f691d184d559bb14ffa1e95
adarshnanda <adarsh.ch.nanda@gmail.com>
2016-06-15 19:58:25 +0530

    15/07: added index.html

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 var http = require('http');
+var fs = require('fs');
 
 http.createServer(onRequest).listen(9000);
 console.log("server running");
 function onRequest(request, response) {
-	console.log("user request" + request.url);
-	response.writeHead(200, {"Context-Type":"text/plain"});
-	response.write("done");
-	response.end();
+	if(request.method == 'GET' && request.url== '/'){
+		response.writeHead(200, {"Context-Type":"text/html"});
+		fs.createReadStream("./index.html").pipe(response);
+	}
+	else{
+		response.writeHead(404,{"Context-Type":"text/html"});
+		fs.createReadStream("./404.html").pipe(response);
+	}
 }
