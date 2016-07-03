a70de7f1f22e6678d849e79868b8e80476cfbc87
adarshnanda <adarsh.ch.nanda@gmail.com>
2016-07-03 23:15:07 +0530

    function

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
 var  myApp = angular.module("myApp", ["myApp2"]);
-myApp.controller('firstCtrl', function (NUMBER) {
-	console.log(NUMBER.ONE);
+myApp.controller('firstCtrl', function ($scope, NUMBER) {
+	console.log('controller loaded');
+	init();
+	function init(){
+		$scope.number1 = 1;
+		$scope.number = 1;
+	}
+	$scope.incr = function () {
+	$scope.number1 = $scope.number + NUMBER.ONE;
+	};
 });
