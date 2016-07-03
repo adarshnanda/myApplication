31ae4cc16f6d1631be25482a53866cc477c7bd17
adarshnanda <adarsh.ch.nanda@gmail.com>
2016-07-03 23:00:01 +0530

    dependency injection and controller

--- app.js
+++ app.js
@@ -1,1 +1,4 @@
-var  myApp = angular.module("myApp", []);
+var  myApp = angular.module("myApp", ["myApp2"]);
+myApp.controller('firstCtrl', function (NUMBER) {
+	console.log(NUMBER.ONE);
+});
---
+++ app1.js
@@ -0,0 +1,4 @@
+var  myApp2 = angular.module("myApp2", []);
+myApp2.constant("NUMBER",{
+"ONE":1
+});
