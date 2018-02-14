31677859acab2c17e210fe111cebe404ac5013f6
Adarsh Nanda <adarsh.nanda@t-mobile.com>
2018-02-14 15:12:57 +0530

    server updated

--- server.js
+++ server.js
@@ -1,15 +1,44 @@
 var http = require('http');
 var fs = require('fs');
+var path = require('path');
+
 
 http.createServer(onRequest).listen(9000);
 console.log("server running");
+
+
 function onRequest(request, response) {
-	if(request.method == 'GET' && request.url== '/'){
-		response.writeHead(200, {"Context-Type":"text/html"});
-		fs.createReadStream("./index.html").pipe(response);
-	}
-	else{
-		response.writeHead(404,{"Context-Type":"text/html"});
-		fs.createReadStream("./404.html").pipe(response);
+	if(request.method == 'GET'){
+    	var filePath = '.' + request.url;
+    	var extname = path.extname(filePath);
+	 	if(request.url== '/'){
+			console.log("index.html served!!");
+			response.writeHead(200, {"Context-Type":"text/html"});
+			fs.createReadStream("./index.html").pipe(response);
+		}else {
+			var contentType = 'text/html';
+    		switch (extname) {
+        	case '.js':
+            	contentType = 'text/javascript';
+            	break;
+        	case '.css':
+            	contentType = 'text/css';
+            	break;
+    		}
+
+
+			fs.readFile(filePath,function(error, content) {
+				if(error){
+					fs.readFile('./404.html', function(error, content) {
+                    	response.writeHead(200, { 'Content-Type': contentType });
+                    	response.end(content, 'utf-8');
+                	});
+				}else{
+					console.log(filePath+" served!!");
+					response.writeHead(200, { 'Content-Type': contentType });
+            		response.end(content, 'utf-8');
+				}
+			});
+		}
 	}
 }
